c71916f5a3e43a2331f47f169b98468b28525213
Muhammed Nazeem <39720973+n4ze3m@users.noreply.github.com>
2022-08-18 13:13:57 +0000

    bug fixed

--- pages/api/keep/link.ts
+++ pages/api/keep/link.ts
@@ -3,7 +3,7 @@ import { database } from "lib/database";
 export default async function handler(req: any, res: any) {
     try {
         const user_id = req.headers.user_id;
-        const data = req.body
+        const data = JSON.parse(req.body);
 
         const query: any = await database.$queryRawUnsafe(`SELECT COALESCE(MAX("sort_no"), 0) + 1 FROM "public"."keep" WHERE "user_id" = uuid($1) AND "keep_type" = 'inbox'`, user_id);
         const sort_no = Number(query[0]["?column?"])
@@ -14,8 +14,7 @@ export default async function handler(req: any, res: any) {
                 sort_no
             }
         })
-
-
+        console.log(data)
         await database.link.create({
             data: {
                 ...data,
@@ -30,7 +29,7 @@ export default async function handler(req: any, res: any) {
 
 
     } catch (e) {
-        console.log("err", e);
+        // console.log("err", e);
         res.status(500).send({
             error: "Something went wrong"
         });
